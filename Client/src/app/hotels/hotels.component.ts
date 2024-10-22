bca572a59a9035f87b65d0d1d31329c79161b75f
MRNULL <20180653@hti.edu.eg>
2024-10-22 20:41:13 +0300

    pagination

--- Client/src/app/hotels/hotels.component.ts
+++ Client/src/app/hotels/hotels.component.ts
@@ -1,20 +1,55 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { HotelsService } from '../_services/hotels.service';
 import { HotelCardComponent } from "../hotel-card/hotel-card.component";
 import { FormsModule, NgModel } from '@angular/forms';
-import { Router } from '@angular/router';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { Rooms } from '../_models/Rooms';
 
 @Component({
   selector: 'app-hotels',
   standalone: true,
-  imports: [HotelCardComponent , FormsModule],
+  imports: [HotelCardComponent , FormsModule, PaginationModule],
   templateUrl: './hotels.component.html',
   styleUrl: './hotels.component.css'
 })
 export class HotelsComponent implements OnInit{
-  
+  paginatedRooms = signal<Rooms[]>([]);
+  currentPage = signal(1);          
+  itemsPerPage = 10;                 
+  totalItems = signal(0);            
   roomService = inject(HotelsService);
-  ngOnInit() {
-    if(!this.roomService.hotels()) this.roomService.loadRooms();    
+
+
+
+  constructor() {
+    effect(() => {
+      const rooms = this.roomService.hotels();
+      if (rooms) {
+        this.totalItems.set(rooms.length);
+        this.paginateRooms();
+      }
+    },{ allowSignalWrites: true });
   }
+
+  ngOnInit() {
+    if(!this.roomService.hotels()) this.roomService.loadRooms();
+  }
+
+
+
+// Function to paginate rooms based on the current page
+paginateRooms(): void {
+  const rooms = this.roomService.hotels();
+  if (rooms) {
+    const startIndex = (this.currentPage() - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedRooms.set(rooms.slice(startIndex, endIndex));
+  }
+}
+
+// Event handler when page changes
+onPageChange(event: any): void {
+  this.currentPage.set(event.page);
+  this.paginateRooms();
+}
 }
